23f1bd660fbfaff14ef669353541f2983f9ba981
Eric Rahm <ericrahm@gmail.com>
2016-01-29 04:17:12 -0800

    Initial draft
    - Adds support for testing Firefox w/ e10s
    - Adds support for testing Chrome
    - Roughly packages things
    - Example script in comp_analysis
    - Works on Windows at least

---
+++ atsy/multitab.py
@@ -0,0 +1,226 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+from marionette.runtests import MarionetteTestRunner
+import mozinfo
+from mozlog.structured import commandline
+import mozprofile
+
+import os
+import shutil
+import time
+
+# Maximum number of tabs to open
+MAX_TABS = 30
+
+# Default amount of seconds to wait in between opening tabs
+PER_TAB_PAUSE = 10
+
+# Default amount of time to wait after loading all tabs.
+SETTLE_WAIT_TIME = 60
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class FirefoxMultiTabTest:
+  """
+  For Firefox we can't use webdriver with e10s enabled, so instead we use
+  marionette directly. See also the test_memory_usage.py file for the
+  actual test implementation.
+
+  This is based on the areweslimyet MarionetteTest.
+  """
+  def run_test(self, binary, stats, urls):
+    testvars = {
+        'perTabPause': PER_TAB_PAUSE,
+        'settleWaitTime': SETTLE_WAIT_TIME,
+        'entities': MAX_TABS,
+        'urls': urls,
+        'stats': stats,
+    }
+
+    e10s = True
+    process_count = 1
+
+    prefs = {
+      # disable network access
+      "network.proxy.socks": "localhost",
+      "network.proxy.socks_port": testvars.get("proxyPort", 3128),
+      "network.proxy.socks_remote_dns": True,
+      "network.proxy.type": 1, # Socks
+
+      # Don't open the first-run dialog, it loads a video
+      'startup.homepage_welcome_url': '',
+      'startup.homepage_override_url': '',
+      'browser.newtab.url': 'about:blank',
+
+      # make sure e10s is enabled
+      "browser.tabs.remote.autostart": e10s,
+      "browser.tabs.remote.autostart.1": e10s,
+      "browser.tabs.remote.autostart.2": e10s,
+      "browser.tabs.remote.autostart.3": e10s,
+      "browser.tabs.remote.autostart.4": e10s,
+      "browser.tabs.remote.autostart.5": e10s,
+      "browser.tabs.remote.autostart.6": e10s,
+      "dom.ipc.processCount": process_count,
+
+      # prevent "You're using e10s!" dialog from showing up
+      "browser.displayedE10SNotice": 1000,
+
+      # override image expiration in hopes of getting less volatile numbers
+      "image.mem.surfacecache.min_expiration_ms": 10000
+    }
+
+    profile = mozprofile.FirefoxProfile(preferences=prefs)
+
+    # TODO(ER): Figure out how to turn on debug level info again
+    #commandline.formatter_option_defaults['level'] = 'debug'
+
+    logger = commandline.setup_logging("MarionetteTest", {})
+    runner = MarionetteTestRunner(
+                    binary=binary,
+                    profile=profile,
+                    logger=logger,
+                    startup_timeout=60)
+
+    # Add our testvars
+    runner.testvars.update(testvars)
+
+    test_path = os.path.join(MODULE_DIR, "test_memory_usage.py")
+    try:
+      print "Marionette - running test"
+      runner.run_tests([test_path])
+      failures = runner.failed
+    except Exception, e:
+      pass
+
+    try:
+      runner.cleanup()
+    except Exception, e:
+      print "Failed to cleanup"
+
+    # cleanup the profile dir if not already cleaned up
+    if os.path.exists(profile.profile):
+      shutil.rmtree(profile.profile)
+
+
+class MultiTabTest:
+    """
+    Many attempts have been made to make this cross-browser. Unfortunately each browser
+    and each OS has its own webdriver deficiencies. The current version seems to work
+    on Chrome.
+
+    Below are a few of the issues as far as I can recall, there are probably
+    more.
+
+    Issues with IE:
+      - It doesn't handle data URLs for HTML.
+      - Ctrl-Shift-Clicking the anchor doesn't open a new tab, it just navigates
+        the current tab.
+      - It can load, but not interact with a file:// URL.
+      - Opening a new tab causes |driver.window_handles| to become empty.
+
+    Issues with Edge:
+      - AFAICT their webdriver does not implement the functionality that we
+        need.
+      - It needs Windows 10, I downloaded a VM but didn't get around to testing
+        given issue #1.
+
+    Issues with Firefox:
+      - Using webdriver w/ e10s enabled is broken.
+      - Sending ActionChains does not work.
+
+    Issues with Chrome:
+      - When selecting a new tab the visual focus is not changed. We work around
+        that by loading a generated page of URLs, Ctrl-Shift-Clicking each
+        anchor tag, the new page is visually loaded, and then we advance to the
+        next tag.
+
+    Issues with Safari:
+      - Safari's webdriver is broken and cannnot be installed in the most
+        recent version of Safari.
+
+    Issues with Opera:
+      - Unknown, I chose not to try Opera given it uses blink and we're already
+        testing Chrome.
+    """
+    def __init__(self, driver, stats):
+        self.driver = driver
+        self.tabs = driver.window_handles
+        self.idx = len(self.tabs) - 1
+        self.stats = stats
+
+        # One of the browsers got grumpy if we didn't load a fake page.
+        # This probably isn't necessary anymore, but might help in a desparate
+        # situation.
+        #self.driver.get('data:text/html,<html><body>Hello!</body></html>')
+
+    def open_tab(self, url):
+        """
+        Opens a new tab and switches focus to it.
+
+        NB: Currently not used because send_keys on the driver doesn't work on OSX
+            for Chrome (at least, might be others).
+        """
+        orig_handles = self.driver.window_handles
+
+        if mozinfo.os == "mac":
+            self.driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + "t")
+        else:
+            self.driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + "t")
+
+        time.sleep(0.25)
+
+        new_handles = set(self.driver.window_handles) - orig_handles
+        new_handle = list(new_handles)[0]
+        self.driver.switch_to_window(new_handle)
+        self.driver.get(url)
+
+        # On Fx at least the handle can change after you load content.
+        new_handles = set(self.driver.window_handles) - orig_handles
+        new_handle = list(new_handles)[0]
+
+        self.tabs.append(new_handle)
+
+    def open_urls_fx(self, urls, per_tab_pause=PER_TAB_PAUSE):
+        """
+        This seemed to work for non-e10s Firefox at one point.
+        """
+        for url in urls:
+            self.open_tab(url)
+            time.sleep(per_tab_pause)
+
+        time.sleep(SETTLE_WAIT_TIME)
+        self.stats.print_stats()
+
+    def open_urls(self, urls,
+        tab_limit=MAX_TABS, per_tab_pause=PER_TAB_PAUSE,
+        settle_wait_time=SETTLE_WAIT_TIME):
+        """
+        This works at least on Chrome across platforms.
+        """
+        # First setup a document with the target URLs.
+        link_doc = 'data:text/html,'
+        link_doc += '<html><head><title>Links!</title></head><body>'
+        id = 0
+        for url in urls:
+            link_doc += '<a id="%d" href="%s">%d: %s</a><br>' % (id, url, id, url)
+            id += 1
+
+        link_doc += '</body><html>'
+        self.driver.get(link_doc)
+
+        # Now open each document in a new tab by ctrl+shift clicking the anchor.
+        for tag in self.driver.find_elements_by_tag_name("a"):
+          action = ActionChains(self.driver)
+
+          if mozinfo.os == "mac":
+            ctrl_key = Keys.COMMAND
+          else:
+            ctrl_key = Keys.CONTROL
+
+          action.key_down(ctrl_key).key_down(Keys.SHIFT).click(tag).key_up(Keys.SHIFT).key_up(ctrl_key).perform()
+          time.sleep(per_tab_pause)
+
+        time.sleep(settle_wait_time)
+        self.stats.print_stats()
---
+++ atsy/test_memory_usage.py
@@ -0,0 +1,155 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+import os
+import sys
+
+from marionette import MarionetteTestCase
+from marionette_driver import Actions
+import mozlog.structured
+from marionette_driver.keys import Keys
+
+# Default amount of tabs to have open at one time.
+MAX_TABS = 30
+
+# Default amount of seconds to wait in between opening tabs
+PER_TAB_PAUSE = 10
+
+# Default amount of seconds to wait for things to be settled down
+SETTLE_WAIT_TIME = 60
+
+class TestMemoryUsage(MarionetteTestCase):
+    """
+    Marionette test case used to open up tabs and measure memory.
+    """
+    def setUp(self):
+        MarionetteTestCase.setUp(self)
+
+        self.marionette.set_context('chrome')
+        self.logger = mozlog.structured.structuredlog.get_default_logger()
+
+        self.urls = self.testvars["urls"]
+        self.pages_to_load = self.testvars.get("entities", len(self.urls))
+        self.perTabPause = self.testvars.get("perTabPause", PER_TAB_PAUSE)
+        self.settleWaitTime = self.testvars.get("settleWaitTime", SETTLE_WAIT_TIME)
+        self.maxTabs = self.testvars.get("maxTabs", MAX_TABS)
+        self.stats = self.testvars.get("stats")
+
+        #self.reset_state()
+
+    def tearDown(self):
+        self.logger.debug("tearing down!")
+        MarionetteTestCase.tearDown(self)
+        self.logger.debug("done tearing down!")
+
+    def reset_state(self):
+        self.pages_loaded = 0
+        self.tabs = self.marionette.window_handles
+        self.marionette.switch_to_window(self.tabs[0])
+
+    def open_and_focus(self):
+        """
+        Opens the next URL in the list and focuses on the tab it is opened in.
+
+        A new tab will be opened if |_maxTabs| has not been exceeded, otherwise
+        the URL will be loaded in the next tab.
+        """
+        page_to_load = self.urls[self.pages_loaded % len(self.urls)]
+        tabs_loaded = len(self.tabs)
+        is_new_tab = False
+
+        if tabs_loaded < self.maxTabs and tabs_loaded <= self.pages_loaded:
+            full_tab_list = self.marionette.window_handles
+
+            # Trigger opening a new tab by finding the new tab button and clicking it
+            newtab_button = (self.marionette.find_element('id', 'tabbrowser-tabs')
+                                            .find_element('anon attribute',
+                                                          {'anonid': 'tabs-newtab-button'}))
+            newtab_button.click()
+
+            self.wait_for_condition(lambda mn: len(mn.window_handles) == tabs_loaded + 1)
+
+            # NB: The tab list isn't sorted, so we do a set diff to determine
+            #     which is the new tab
+            new_tab_list = self.marionette.window_handles
+            new_tabs = list(set(new_tab_list) - set(full_tab_list))
+
+            self.tabs.append(new_tabs[0])
+            tabs_loaded += 1
+
+            is_new_tab = True
+
+        tab_idx = self.pages_loaded % self.maxTabs
+
+        tab = self.tabs[tab_idx]
+
+        # Tell marionette which tab we're on
+        # NB: As a work-around for an e10s marionette bug, only select the tab
+        #     if we're really switching tabs.
+        if tabs_loaded > 1:
+          self.logger.debug("switching to tab")
+          self.marionette.switch_to_window(tab)
+          self.logger.debug("switched to tab")
+
+        with self.marionette.using_context('content'):
+            self.logger.info("loading %s" % page_to_load)
+            self.marionette.navigate(page_to_load)
+            self.logger.debug("loaded!")
+
+        # On e10s the tab handle can change after actually loading content
+        if is_new_tab:
+          # First build a set up w/o the current tab
+          old_tabs = set(self.tabs)
+          old_tabs.remove(tab)
+          # Perform a set diff to get the (possibly) new handle
+          [new_tab] = set(self.marionette.window_handles) - old_tabs
+          # Update the tab list at the current index to preserve the tab ordering
+          self.tabs[tab_idx] = new_tab
+
+        # give the page time to settle
+        time.sleep(self.perTabPause)
+
+        self.pages_loaded += 1
+
+    def signal_user_active(self):
+        """Signal to the browser that the user is active.
+
+        Normally when being driven by marionette the browser thinks the
+        user is inactive the whole time because user activity is
+        detected by looking at key and mouse events.
+
+        This would be a problem for this test because user inactivity is
+        used to schedule some GCs (in particular shrinking GCs), so it
+        would make this unrepresentative of real use.
+
+        Instead we manually cause some inconsequential activity (a press
+        and release of the shift key) to make the browser think the user
+        is active.  Then when we sleep to allow things to settle the
+        browser will see the user as becoming inactive and trigger
+        appropriate GCs, as would have happened in real use.
+        """
+        action = Actions(self.marionette)
+        action.key_down(Keys.SHIFT)
+        action.key_up(Keys.SHIFT)
+        action.perform()
+
+    def test_open_tabs(self):
+        """
+        Opens tabs, measures memory.
+        """
+        self.logger.info("test_open_tabs")
+
+        # This gives Firefox time to get into an OK state.
+        time.sleep(10)
+
+        self.reset_state()
+
+        for x in range(self.pages_to_load):
+            self.open_and_focus()
+            self.signal_user_active()
+
+        time.sleep(self.settleWaitTime)
+
+        self.stats.print_stats()
